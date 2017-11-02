e4b329a4c4455f8d11b891ae69d5f660a8dd16d4
aragakiyui <heng8050lee@foxmail.com>
2017-11-02 04:11:08 +0000

    Finish basic page layouts

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 <a href="#" target="_blank">Aar</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="https://github.com/DowneyL">我的 GitHub</a></p>
+    </div>
+</footer>
