dd0a6898e56c2af953cfd62bfd742226c38c9ab0
aragakiyui <heng8050lee@foxmail.com>
2017-11-03 02:02:29 +0000

    Generated topics

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 
 // 用户资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
