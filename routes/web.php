ad13630a3efbc27f51938ccd6253fbf0cca95a9c
aragakiyui <heng8050lee@foxmail.com>
2017-11-07 01:48:37 +0000

    Auto translate slug

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,8 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset');
 // 用户资源路由
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
-Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::get('topic/{topic}/{slug?}', 'TopicsController@show')->name('topics.show');
 
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 
