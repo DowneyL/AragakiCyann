11510ada5380136bcb741205a7c602462575fffd
aragakiyui <heng8050lee@foxmail.com>
2017-11-15 07:58:59 +0000

    fixbug

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => $password ?: $password = bcrypt('123456'),
+        'password' => $password ?: $password = bcrypt('panzer950805scc'),
         'remember_token' => str_random(10),
         'introduction' => $faker->sentence(),
         'created_at' => $now,
